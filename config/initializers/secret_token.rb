90d5a286d95c3940eae6ba8a07e3a50738e6a405
NikantVohra <nikant@outlook.com>
2014-05-01 11:37:36 +0530

    initializing the app with a user model

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Splitpay::Application.config.secret_key_base = '6545cc9ab2bd685129f0709d3e228672a9efc905e08ac73e6a38f95848420a16eab86781033bdf007f7e6568bb096d1d2926c45edfa0bd404dfd88df5b196eee'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Splitpay::Application.config.session_store :cookie_store, key: '_Splitpay_session'
